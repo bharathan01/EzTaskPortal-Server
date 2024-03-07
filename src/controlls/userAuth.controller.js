8c8ef0e6e02b4a3754999018f9a73cfd4094c3c6
bharathan01 <bharathandileep01@gmail.com>
2024-03-08 00:35:03 +0530

    error handler update

--- src/controlls/userAuth.controller.js
+++ src/controlls/userAuth.controller.js
@@ -1,13 +1,16 @@
 const router = require("../routes/user.route.js");
 const errorHandler = require("../utils/errorHander.js");
 
-const registerNewUser = async (req, res) => {
- 
+const registerNewUser = (req, res) => {
+  try {
     const body = req.body;
     if (!body.email) {
       throw new errorHandler(400, "email is require");
     }
-    return res.status(200).json(body)
+    return res.status(200).json(body);
+  } catch (error) {
+     return res.status(error.statusCode || 500).json({ error: error.message });
+  }
 };
 
 module.exports = registerNewUser;
